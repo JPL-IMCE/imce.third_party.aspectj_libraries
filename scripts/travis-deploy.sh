5a9ba83fc94f66e57967f6e297b1d890e54ca750
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-08-31 16:48:56 +0000

    Travis CI, Bintray

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -10,6 +10,7 @@ set -ev
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out local.secring.gpg -d
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out local.pubring.gpg -d
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in credentials.sbt.enc -out local.credentials.sbt -d
+openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in deploy_key.enc -out local.deploy_key -d
 
 chmod 600 local.*
 eval `ssh-agent -s`
